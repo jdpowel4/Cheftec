91fc1664f9d032bec09af5929af835e875dcd9e1
Jeremy Powell <jeremy_powell@outlook.com>
2026-01-10 17:28:52 -0500

    new file:   core/database.py 	new file:   models/base.py

---
+++ models/base.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, DateTime, func
+from app.core.database import Base
+
+class TimestampMixin:
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
